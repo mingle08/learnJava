7340b29fb428434bf71ccff2e95c55b06cf46f41
mingle08 <mingle08@163.com>
2018-01-01 23:26:59 +0800

    美女老师讲的插入算法

---
+++ algoDemo/InsertionSort/InsertSortApp.java
@@ -0,0 +1,25 @@
+package algorithms.beauty;
+
+public class InsertSortApp {
+
+	public static void main(String[] args) {
+		int maxSize = 100;
+		ArrayIns arr;
+		arr = new ArrayIns(maxSize);
+		arr.insert(77);
+		arr.insert(99);
+		arr.insert(55);
+		arr.insert(44);
+		arr.insert(22);
+		arr.insert(88);
+		arr.insert(11);
+		arr.insert(0);
+		arr.insert(66);
+		arr.insert(33);
+		arr.display();
+		arr.insertionSort();
+		arr.display();
+
+	}
+
+}
